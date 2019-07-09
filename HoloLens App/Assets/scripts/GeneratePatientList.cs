c5ce86b99d300fc4209e62fb1df719dd91a8db6a
fanbomeng97 <46536567+fanbomeng97@users.noreply.github.com>
2019-07-09 14:28:11 +0100

    Add script to load patient list

---
+++ HoloLens App/Assets/scripts/GeneratePatientList.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratePatientList : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject buttonTemplates;
+
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject button = Instantiate(buttonTemplates) as GameObject;
+            button.SetActive(true);
+
+            //button.GetComponent<>().SetText("patient name: Alice");
+
+            button.transform.SetParent(buttonTemplates.transform.parent, false);
+        }
+    }
+}
