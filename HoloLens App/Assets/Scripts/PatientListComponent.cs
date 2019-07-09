ede9689b946234a06f10b53fac171990d2eab81a
fanbomeng97 <46536567+fanbomeng97@users.noreply.github.com>
2019-07-09 18:07:36 +0100

    Upadting

---
+++ HoloLens App/Assets/Scripts/PatientListComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PatientListComponent : MonoBehaviour
+{
+    [SerializeField]
+    private Text listText;
+    private TextMeshProUGUI Text;
+
+    public void SetText(string mytext)
+    {
+        listText.text = mytext;
+    }
+}
